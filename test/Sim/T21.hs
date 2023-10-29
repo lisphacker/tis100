4ce2157185c138dd76cb53a44a65fbee12ca3a78
Gautham Ganapathy <gauthamg@gmail.com>
2023-10-29 13:13:21 +0000

    Added tests for ADD

--- test/Sim/T21.hs
+++ test/Sim/T21.hs
@@ -1,5 +1,6 @@
 module Sim.T21 where
 
+import Control.Monad (forM_)
 import Data.Vector qualified as V
 import TIS100.Tiles.Base
 import TIS100.Tiles.ConnectedTile (step)
@@ -21,6 +22,40 @@ mkT21Tile initAcc initBak instns =
           , runState = Ready
           }
     }
+
+testADD :: Bool -> Spec
+testADD add = describe ("Testing " ++ insName) $ do
+  testADD_ACC
+  testADD_BAK
+  forM_ [UP, DOWN, LEFT, RIGHT] testADD_Port
+ where
+  testADD_ACC = do
+    let init = mkT21Tile 10 20 [if add then ADD (Register ACC) else SUB (Register ACC)]
+    let next = step init
+
+    describe ("Testing " ++ insName ++ " ACC") $ do
+      it "Status" $ do
+        acc (tileState next) `shouldBe` Value (f 10 10)
+
+  testADD_BAK = do
+    let init = mkT21Tile 10 20 [if add then ADD (Register BAK) else SUB (Register BAK)]
+    let next = step init
+
+    describe ("Testing " ++ insName ++ " ACC") $ do
+      it "Status" $ do
+        acc (tileState next) `shouldBe` Value (f 10 20)
+
+  testADD_Port port = do
+    let init = mkT21Tile 10 20 [if add then ADD (Port port) else SUB (Port port)]
+    let next = step init
+
+    describe ("Testing " ++ insName ++ " " ++ show port) $ do
+      it "Status" $ do
+        acc (tileState next) `shouldBe` Value 10
+        runState (tileState next) `shouldBe` WaitingOnRead port Nothing
+
+  f = if add then (+) else (-)
+  insName = if add then "ADD" else "SUB"
 
 testADDI :: Spec
 testADDI = do
@@ -51,6 +86,8 @@ testNOP = do
 
 simTestsSpec :: Spec
 simTestsSpec = describe "Intra-T21 tests" $ parallel $ do
+  testADD True
+  testADD False
   testADDI
   testMOVI
   testNOP
